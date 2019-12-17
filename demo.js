1208ef1072695dc9ead36bf6310fd01ad2410254
Devin McFarlane <devin.ryan.mcfarlane@gmail.com>
2019-12-17 14:12:19 -0500

    FRANCO

---
+++ demo.js
@@ -0,0 +1,7 @@
+function buttonClicked() {
+    if (document.getElementById("para").style.opacity == 1){
+        document.getElementById("para").style.opacity = 0;
+    } else {
+        document.getElementById("para").style.opacity = 1;
+    }
+}
